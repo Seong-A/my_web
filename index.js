78220abc7d4d7c761426caa633ce72aa0a0ec4b8
Seong-A <ksa6777@naver.com>
2023-07-27 17:16:02 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+const config = require('./config/key');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://Seong-A:tjddk2514@seeing-a.xulub9e.mongodb.net/?retryWrites=true&w=majority', {
-    useNewUrlParser: true
-}).then(() => console.log('MongoDB Connected...'))
-    .catch(err => console.log(err));
+mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true, // 이 옵션을 추가해 주세요.
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello World! 하욤'));
+app.get('/', (req, res) => res.send('Hello World! 야호'));
+
+app.post('/register', async (req, res) => { // 비동기 함수로 변경합니다.
+
+  try {
+    const user = new User(req.body);
+
+    await user.save(); // await를 사용하여 프로미스를 처리합니다.
+
+    return res.status(200).json({
+      success: true
+    });
+  } catch (err) {
+    return res.json({ success: false, err });
+  }
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
